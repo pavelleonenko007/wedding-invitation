7f4d03f8876468ed8be67a1dbd0a00bd35462a11
pavelleonenko007 <pavel.leonenko374@gmail.com>
2024-01-19 20:58:07 +0300

    fix: container wrapper

--- components/Container.tsx
+++ components/Container.tsx
@@ -1,3 +1,3 @@
 export default function Container({ children }: { children: React.ReactNode }) {
-	return <div className="w-full max-w-[1280px] mx-auto">{children}</div>;
+	return <div className="container">{children}</div>;
 }
