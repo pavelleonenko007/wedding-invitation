eb924294d5014c4f1676db9faefc7438d8d4886b
pavelleonenko007 <pavel.leonenko374@gmail.com>
2024-01-25 14:41:48 +0300

    fix: title and description on dashboard page

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,6 +1,12 @@
 import Container from '@/components/Container';
 import { GuestsTable } from '@/components/GuestsTable';
+import type { Metadata } from 'next';
 import { getGuests } from './actions';
+
+export const metadata: Metadata = {
+	title: 'Панель управления гостями',
+	description: 'Здесь можно добавлять гостей и изменять уже существующих',
+};
 
 export default async function index() {
 	// unstable_noStore();
