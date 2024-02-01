8f8a38584716946abc3f7d48e15c33bd162f5f60
pavelleonenko007 <pavel.leonenko374@gmail.com>
2024-02-01 12:29:01 +0300

    feat: install navigation component

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,14 +1,41 @@
 'use client';
 import { signOut, useSession } from 'next-auth/react';
 import Link from 'next/link';
+import {
+	NavigationMenu,
+	NavigationMenuItem,
+	NavigationMenuLink,
+	NavigationMenuList,
+	navigationMenuTriggerStyle,
+} from './ui/navigation-menu';
 
 export default function Navbar() {
 	const session = useSession();
 	return (
-		<div className="flex justify-center gap-2 w-full p-4 fixed top-0 left-0 bg-white border-b-[1px] border-black">
-			<Link href={'/'}>Home</Link>
-			<Link href={'/dashboard'}>Dashboard</Link>
-			{session.data?.user && <Link href={'#'} onClick={() => signOut()}>Sing Out</Link>}
+		<div className="w-full py-2 sm:py-4">
+			<NavigationMenu>
+				<NavigationMenuList>
+					<NavigationMenuItem>
+						<Link href="/" legacyBehavior passHref>
+							<NavigationMenuLink className={navigationMenuTriggerStyle()}>
+								Главная
+							</NavigationMenuLink>
+						</Link>
+					</NavigationMenuItem>
+					{session.data?.user && (
+						<NavigationMenuItem>
+							<Link href={'#'} legacyBehavior passHref>
+								<NavigationMenuLink
+									onClick={() => signOut()}
+									className={navigationMenuTriggerStyle()}
+								>
+									Выйти
+								</NavigationMenuLink>
+							</Link>
+						</NavigationMenuItem>
+					)}
+				</NavigationMenuList>
+			</NavigationMenu>
 		</div>
 	);
 }
